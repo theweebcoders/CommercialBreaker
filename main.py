9506856d4445628a8ff3f9565668ee343de2b88f
tim000x3 <timeister@mac.com>
2024-04-25 02:47:12 -0400

    added CommercialBreakerCLI

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 from GUI import TOM, CommercialBreaker
-from CLI import clydes
+from CLI import clydes, CommercialBreakerCLI
 
 def main():
     parser = argparse.ArgumentParser(description="Run different parts of the application.")
@@ -8,11 +8,11 @@ def main():
     group.add_argument('--tom', action='store_true', help="Run the GUI interface (TOM)")
     group.add_argument('--clydes', action='store_true', help="Run the CLI interface")
     group.add_argument('--combreak', action='store_true', help="Run the standalone Commercial Breaker GUI")
-
+    group.add_argument('--combreakcli', action='store_true', help="Run the CLI interface for the Commercial Breaker")
     args = parser.parse_args()
 
     # Default to TOM if no arguments are given
-    if not (args.clydes or args.combreak):
+    if not (args.clydes or args.combreak or args.combreakcli):
         args.tom = True
 
     if args.tom:
@@ -21,6 +21,8 @@ def main():
         clydes()  # Run the CLI interface
     elif args.combreak:
         CommercialBreaker()  # Run the Commercial Breaker GUI
+    elif args.combreakcli:
+        CommercialBreakerCLI()
 
 if __name__ == "__main__":
     main()
